ccbfee591015d7a5f4feb04adcb2f32ba2b32822
yjs1213 <jidbyoo@gmail.com>
2022-01-03 22:47:04 +0900

    충돌 감지 기능 추가

--- AccidentHere/ViewController.swift
+++ AccidentHere/ViewController.swift
@@ -6,19 +6,30 @@
 //
 import UIKit
 import CoreLocation
+import CoreMotion
+import AVFoundation
 
 class ViewController: UIViewController, CLLocationManagerDelegate{
     
     var locationManager: CLLocationManager = CLLocationManager()
-
-    var baseline = 10.0
-
+    var audioPlayer : AVAudioPlayer!
+    let motion = CMMotionManager()
+    var audioFile : URL!
+    var baseline = 10.0  // 충돌 감지가 시작되는 최소 속도
+    var collisionSensitivity = 8.0 // 충돌 감지의 민감도 (낮을수록 민감)
+    
     @IBOutlet var speedLabel: UILabel!
     @IBOutlet var statusLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        statusLabel.textColor = UIColor.systemGray5
+        audioFile = Bundle.main.url(forResource: "Blop Sound", withExtension: "mp3")
+        
+        initAccelerometer()
+        initPlay()
+        
         // Ask for Authorisation from the User.
         self.locationManager.requestAlwaysAuthorization()
 
@@ -48,14 +59,48 @@ class ViewController: UIViewController, CLLocationManagerDelegate{
         if (speed > 0) {
             if (speed > baseline) {
                 self.view.backgroundColor = UIColor.red
+                self.statusLabel.textColor = UIColor.white
+                self.motion.startAccelerometerUpdates()
             } else {
                 self.view.backgroundColor = UIColor(named: "backgroundColor")
+                self.motion.stopAccelerometerUpdates()
             }
             
             speedLabel.text = (String(format: "%.0f", speed))
         } else {
             speedLabel.text = "0"
         }
+    }
+    
+    func initAccelerometer(){
+        if motion.isAccelerometerAvailable {
+            motion.accelerometerUpdateInterval = 1.0 / 60.0
+            
+            let sensitivityValue = self.collisionSensitivity
+            let timer = Timer(fire: Date(), interval: (1.0/60.0), repeats: true, block: { (timer) in
+                if let data = self.motion.accelerometerData {
+                    let val_x = data.acceleration.x
+                    let val_y = data.acceleration.y
+                    let val_z = data.acceleration.z
+                    
+                    if val_x > sensitivityValue || val_y > sensitivityValue || val_z > sensitivityValue{
+                        print("충돌 감지")
+                        print("X : \(val_x) Y : \(val_y) Z : \(val_z)")
+                        self.audioPlayer.play()
+                    }
+                }
+            })
+            RunLoop.current.add(timer, forMode: RunLoop.Mode.default)
+        }
+    }
+    
+    func initPlay(){
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOf: audioFile)
+        } catch let error as NSError{
+            print("Error Initial Play")
+        }
+        audioPlayer.prepareToPlay()
     }
 }
 
